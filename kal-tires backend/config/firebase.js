16dec719e63552844233eda5aa2b544fd10410de
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2024-08-17 01:35:36 +0000

    re-commit

--- kal-tires backend/config/firebase.js
+++ kal-tires backend/config/firebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../ServiceKey.json");
+var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
